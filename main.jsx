40a8a49741b4f57c1675edba2999fd71ea711740
safakmirza <safakmirza@gmail.com>
2026-03-07 16:19:02 +0300

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './door-tracker-firebase.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
